7fe75664230bc86b80f4bb51da2346a71f2c8853
Maximilian Walsh <max@walsh.gg>
2023-08-25 05:52:18 +0200

    Testing basic JS functions

---
+++ Testing/test.js
@@ -0,0 +1,16 @@
+// test.js
+
+// Logs a message
+console.log("Javascript javascript javascript");
+console.log("test")
+
+// Function to add two numbers
+function addNumbers(a, b) {
+  return a + b;
+}
+
+// Testing the addNumbers function
+var num1 = 5;
+var num2 = 10;
+var result = addNumbers(num1, num2);
+console.log(`The sum of ${num1} and ${num2} is ${result}`);
